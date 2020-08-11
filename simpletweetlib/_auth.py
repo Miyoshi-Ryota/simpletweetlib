1ea0bbf769efd3ed3991f61306e94e5d54f5ff34
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2020-08-11 19:24:56 +0900

    Fix pep8 errors

--- simpletweetlib/_auth.py
+++ simpletweetlib/_auth.py
@@ -3,7 +3,10 @@
 from requests_oauthlib import OAuth1Session
 
 
-def get_auth_session(consumer_key, consumer_secret, access_token, access_token_secret):
+def get_auth_session(consumer_key,
+                     consumer_secret,
+                     access_token,
+                     access_token_secret):
     return OAuth1Session(consumer_key,
                          client_secret=consumer_secret,
                          resource_owner_key=access_token,
--- simpletweetlib/tweet.py
+++ simpletweetlib/tweet.py
@@ -1,6 +1,6 @@
 # coding: utf-8
-
-import auth
+"""Tweet Client"""
+import _auth
 
 
 class Tweet:
@@ -8,12 +8,18 @@ class Tweet:
     t = Tweet(consumer_key, consumer_secret, access_token, access_token_secret)
     t.tweet('test tweet by twitter api')
     """
-    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
-        self.twitter = auth.get_auth_session(consumer_key,
-                                             consumer_secret,
-                                             access_token,
-                                             access_token_secret)
+
+    def __init__(self,
+                 consumer_key,
+                 consumer_secret,
+                 access_token,
+                 access_token_secret):
+        self.twitter = _auth.get_auth_session(consumer_key,
+                                              consumer_secret,
+                                              access_token,
+                                              access_token_secret)
 
     def tweet(self, msg):
-        self.twitter.post("https://api.twitter.com/1.1/statuses/update.json?status={}".format(msg))
-
+        self.twitter.post(
+            "https://api.twitter.com/1.1/statuses/update.json?status={}"
+            .format(msg))
